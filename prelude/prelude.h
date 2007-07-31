91c673b951a825556008b41536251c32dc5e1fb1
Eelis van der Weegen <eelis@eelis.net>
2007-08-01 01:25:48 +0200

    More powerful cast_dammit_cast, and new truly vicious savage_cast.
    darcs-hash:20070731232548-de023-691a2486559435ccf0d37fa5a3afabd081e4e135.gz

--- prelude/prelude.h
+++ prelude/prelude.h
@@ -116,8 +116,19 @@ std::string advice ();
 template <typename T, size_t N> size_t array_size (T (&)[N]) { return N; }
   // Redundant. boost::size from Boost.Range handles these.
 
-template <typename T, typename T2> T cast_dammit_cast (T2 const what) { return *((T*) &what); }
-  // Doesn't actually work with strict aliasing rules enforced.
+// Evil casts:
+
+  // Consider   some_evil_cast<char const *>("foo")   . There are two possible interpretations for the result: either the array should decay into a pointer and be immediately returned, or the bytes making up the array are interpreted as the bytes making up the to-be-returned pointer. cast_dammit_cast does the former, while savage_cast does the latter.
+
+  template <typename To, typename From> To savage_cast (From const & from)
+  { return (To const &) from; }
+    // Can even be used to check endianness:  geordi << hex << savage_cast<uint32_t>("\xef\xbe\xad\xde"))
+
+  template <typename To, typename From> To cast_dammit_cast (From const from)
+  {
+    From const & r = from; // from itself is not a reference because we want arrays to decay, so that cast_dammit_cast<char*>("oi") works properly.
+    return (To const &) r;
+  }
 
 typedef unsigned char uchar;
 typedef unsigned int uint;
