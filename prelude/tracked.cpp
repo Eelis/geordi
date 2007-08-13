bc873aed9fdf0e320bc054a2b8e0bc951769547a
Eelis van der Weegen <eelis@eelis.net>
2007-08-13 04:23:41 +0200

    Tracked: more succinct display, less redundant implementation.
    darcs-hash:20070813022341-ec8c9-8ab7ecf1ee557b709e3ec6803478f37b0436ff06.gz

--- prelude/tracked.cpp
+++ prelude/tracked.cpp
@@ -3,147 +3,54 @@
 
 #include "foreach.hpp"
 
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <cstdio>
-
-#include <boost/static_assert.hpp>
-#include <boost/io/ios_state.hpp>
-
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-#include <utility> //move
-#endif
-
 namespace tracked
 {
-  namespace
+  namespace detail
   {
-    template <typename T>
-    std::ostream & prt (T const * const t) { return std::cout << ' ' << *t << "::"; }
+    unsigned int Idd::new_id = 0;
 
-    void print_addr (std::ostream & o, void const * const p)
+    Idd::Idd (): id(new_id++), pillaged(false)
     {
-      boost::io::ios_flags_saver const fs (o);
-      typedef unsigned long ulong;
-      BOOST_STATIC_ASSERT(sizeof(ulong) == sizeof(void const *));
-      ulong const i = ulong(p);
-
-      ulong const bound =
-        #ifdef __x86_64__
-          0x1000000000;
-        #else
-          0x10000000;
-        #endif
-
-      o << (i < bound ? 'H' : 'S') << std::hex << std::setfill('0') << std::setw(2) << (i & 0xfful);
-        // Stack/heap distinction based on pretty shameless assumption that just happens to hold on tested machines.
+      reg.s.insert(this);
+      for (Allocations::iterator i = allocations.begin(); i != allocations.end(); ++i)
+        if (i->first.first <= this && this < i->first.second) { i->second.insert(id); break; }
     }
-  }
 
-  // B:
+    Idd::Idd (Idd const & i): id(new_id++), pillaged(false)
+    { i.nopillage("copy"); reg.s.insert(this); }
 
-    B::B (): pillaged(false) { reg.s.insert(this); prt(this) << "B() "; }
+    Idd & Idd::operator= (Idd const & i)
+    { i.nopillage("assign from"); pillaged = false; return *this; }
 
-    #ifdef __GXX_EXPERIMENTAL_CXX0X__
-      B & B::operator= (B && b) { 
-        b.nopillage("move-assign from");
-        b.pillaged = true;
-        pillaged = false;
-        std::cout << ' ' << *this << "<=" << b << ' '; return *this; 
-      }
-
-      B::B (B && b): pillaged(false)
-      {
-        b.nopillage("move from");
-        b.pillaged = true;
-        reg.s.insert(this);
-        std::cout << ' ' << b << "=>" << *this << ' '; // '=>' for "move"
-      }
-    #endif
-
-    B::B (int const i): pillaged(false) { reg.s.insert(this); prt(this) << "B(" << i << ") "; }
-
-    void B::nopillage (char const * const s) const
+    void Idd::nopillage (char const * const s) const
     {
       if (!pillaged) return;
-      std::cout << " Error: Tried to " << s << " pillaged " << *this << '.' << std::flush;
+      std::cout << " Error: Tried to " << s << " pillaged " << name() << id << '.' << std::flush;
       std::fclose(stdout); // Otherwise we get leak messages.
       std::exit(0);
     }
 
-    B::B (B const & b): pillaged(false)
-    {
-      b.nopillage("copy");
-      reg.s.insert(this); prt(this) << "B(" << b << ") ";
-    }
+    #ifdef __GXX_EXPERIMENTAL_CXX0X__
 
-    B & B::operator= (B const & b)
-    {
-      b.nopillage("assign from");
-      pillaged = false;
-      std::cout << ' ' << *this << '=' << b << ' '; return *this;
-    }
+      Idd::Idd (Idd && i): id(new_id++), pillaged(false)
+      { i.nopillage("move"); i.pillaged = true; reg.s.insert(this); }
 
-    B & B::operator++ () { nopillage("pre-increment"); std::cout << " ++" << *this << ' '; return *this; }
-    B B::operator++ (int) { nopillage("post-increment"); B const r (*this); operator++(); return r;  }
+      Idd & Idd::operator= (Idd && i)
+      { i.nopillage("move-assign from"); pillaged = false; i.pillaged = true; return *this; }
 
-    void B::f () const { nopillage("call B::f() on"); prt(this) << "f() "; }
-    void B::vf () const { nopillage("call B::vf() on"); prt(this) << "vf() "; }
+    #endif
 
-    B::~B () { reg.s.erase(this); prt(this) << "~B() "; }
+    Idd::~Idd () { reg.s.erase(this); }
 
-    void * B::operator new (size_t const s) { void * r = ::operator new(s); std::cout << " new"; return r; }
-    void B::operator delete (void * p) { std::cout << "deleted "; ::operator delete(p); }
+    Allocations allocations;
 
-    void * B::operator new[] (size_t const s) { void * r = ::operator new[](s); std::cout << " new[]"; return r; }
-    void B::operator delete[] (void * p) { std::cout << "delete[]d "; ::operator delete[](p); }
+    Idd::Reg Idd::reg;
 
-      // These new/delete operators are not called when B's are new'd as part of bigger objects, so B's ctors/dtor are still the appropriate place for reg insertion/erasure.
-
-    B::Reg B::reg;
-
-    B::Reg::~Reg ()
+    Idd::Reg::~Reg ()
     {
       if (s.empty()) return;
       std::cout << " || leaked:";
-      BOOST_FOREACH(B const * const b, s) std::cout << ' ' << *b;
+      BOOST_FOREACH(Idd const * const b, s) std::cout << ' ' << b->name() << b->id;
     }
-
-    std::ostream & operator<< (std::ostream & o, B const & b) { o << "B@"; print_addr(o, &b); return o; }
-
-  // D:
-
-    D::D () { prt(this) << "D() "; }
-    D::D (D const & d): B(d) { prt(this) << "D(" << d << ") "; }
-    #ifdef __GXX_EXPERIMENTAL_CXX0X__
-      D::D (D && d) { std::cout << ' ' << d << "=>" << *this << ' '; }      
-      D & D::operator= (D && d) { B::operator=(std::move(d)); std::cout << ' ' << *this << "<=" << d << ' '; return *this; }
-    #endif
-
-    D & D::operator= (D const & d)
-    { B::operator=(d); std::cout << ' ' << *this << '=' << d << ' '; return *this; }
-
-    void D::vf () const { prt(this) << "vf() "; }
-
-    D::~D () { prt(this) << "~D() "; }
-
-    std::ostream & operator<< (std::ostream & o, D const & d) { o << "D@"; print_addr(o, &d); return o; }
-
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-    void swap(B & a, B & b) {
-        B tmp(std::move(b));
-        b = std::move(a);
-        a = std::move(tmp);
-    }
-    void swap(B && a, B & b) { b = std::move(a); }
-    void swap(B & a, B && b) { a = std::move(b); }
-    void swap(D & a, D & b) {
-        D tmp(std::move(b));
-        b = std::move(a);
-        a = std::move(tmp);
-    }
-    void swap(D && a, D & b) { b = std::move(a); }
-    void swap(D & a, D && b) { a = std::move(b); }
-#endif
+  }
 }
--- prelude/tracked.hpp
+++ prelude/tracked.hpp
@@ -1,65 +1,140 @@
 
-#include <iosfwd>
+#ifndef TRACKED_HPP
+#define TRACKED_HPP
+
+#include <iostream>
 #include <set>
+#include <map>
+#include <cstdlib>
+#include <cstdio>
+#include <cassert>
+#include <utility>
 
 namespace tracked
 {
-  struct B
+  namespace detail
   {
-    B ();
-    explicit B (int);
+    struct Idd
+    {
+      Idd ();
+      Idd (Idd const &);
+      Idd & operator= (Idd const &);
+
+      #ifdef __GXX_EXPERIMENTAL_CXX0X__
+        Idd (Idd &&);
+        Idd & operator= (Idd &&);
+      #endif
+
+      ~Idd ();
+      unsigned int const id;
+
+      protected:
+
+        void nopillage (char const *) const;
+
+      private:
+
+        bool pillaged;
+
+        virtual char name () const = 0;
+
+        static unsigned int new_id;
+        static struct Reg { std::set<Idd const *> s; ~Reg (); } reg;
+    };
+
+    typedef std::map<std::pair<void const *, void const *>, std::set<unsigned int> > Allocations;
+    extern Allocations allocations;
+
+    template <typename Base, char Name>
+    struct T: Base
+    {
+      T () { std::cout << ' ' << *this << "* "; }
+
+      explicit T (int const i) { std::cout << ' ' << *this << "*(" << i << ") "; }
+
+      T (T const & t): Base(t) { std::cout << ' ' << *this << "*(" << t << ") "; }
+
+      T & operator= (T const & t)
+      { Base::operator=(t); std::cout << ' ' << *this << '=' << t << ' '; return *this; }
+
+      #ifdef __GXX_EXPERIMENTAL_CXX0X__
+
+        // Moves are displayed as =>, because -> and <= are operators.
+
+        T (T && t): Base(std::move(t)) { std::cout << ' ' << t << "=>" << *this << "* "; }
+
+        T & operator= (T && t)
+        { Base::operator=(std::move(t)); std::cout << ' ' << t << "=>" << *this << ' '; return *this; }
+
+      #endif
+
+      T & operator++ () { Base::nopillage("pre-increment"); std::cout << " ++" << *this << ' '; return *this; }
+      T operator++ (int) { Base::nopillage("post-increment"); T const r (*this); operator++(); return r;  }
+
+      void f () const { Base::nopillage("call T::f() on"); std::cout << ' ' << *this << ".f() "; }
+      virtual void vf () const { Base::nopillage("call T::vf() on"); std::cout << ' ' << *this << ".vf() "; }
+
+      virtual ~T () { std::cout << ' ' << *this << "~ "; }
+
+      void * operator new (size_t const s) { return op_new(s, false); }
+      void * operator new[] (size_t const s) { return op_new(s, true); }
+      void operator delete (void * const p) { op_delete(p, false); }
+      void operator delete[] (void * const p) { op_delete(p, true); }
+
+      private:
+
+        static void * op_new (size_t const s, bool const array)
+        {
+          void * const r = ::operator new(s);
+          allocations.insert(std::make_pair(std::make_pair(r, static_cast<char *>(r) + s), std::set<unsigned int>()));
+          std::cout << " new(" << Name << (array ? "[]" : "") << ") ";
+          return r;
+        }
+
+        static void op_delete (void * const p, bool const array)
+        {
+          Allocations::const_iterator i = allocations.begin();
+          for (; i != allocations.end(); ++i) if (i->first.first == p) break;
+          assert(i != allocations.end());
+          std::set<unsigned int> const & ids (i->second);
+          std::cout << " delete";
+          if (array) {
+            typename std::set<unsigned int>::const_iterator b = ids.begin(), e = ids.end();
+            std::cout << '[';
+            if (b != e) { std::cout << Name << *b; while (++b != e) std::cout << ',' << Name << *b; }
+            std::cout << ']';
+          } else {
+            assert(ids.size() == 1);
+            std::cout << '(' << Name << *(ids.begin()) << ')';
+          }
+          std::cout << ' ';
+          ::operator delete(p);
+        }
+
+        char name () const { return Name; }
+    };
+
+    template <typename B, char N>
+    std::ostream & operator<< (std::ostream & o, T<B, N> const & t) { return o << N << t.id; }
+
     #ifdef __GXX_EXPERIMENTAL_CXX0X__
-      B (B &&);
-      B & operator= (B &&);
+
+      // This "three moves" swap implementation is most likely identical to what libstdc++'s std::swap will be using, so once libstdc++'s std::swap has been updated, the functions below can be removed.
+
+      template <typename B, char N>
+      void swap(T<B, N> & a, T<B, N> & b)
+      { T<B, N> tmp(std::move(b)); b = std::move(a); a = std::move(tmp); }
+
+      template <typename B, char N> void swap(T<B, N> && a, T<B, N> & b) { b = std::move(a); }
+      template <typename B, char N> void swap(T<B, N> & a, T<B, N> && b) { a = std::move(b); }
+
     #endif
-    B (B const &);
-    B & operator= (B const &);
-    virtual ~B ();
 
-    B & operator++ ();
-    B operator++ (int);
-      // Allows for a nice demonstration of the canonical postfix operator++ semantics using { tracked::B s (2); (s++).f(); }
+  } // namespace detail
 
-    void f () const;
-    virtual void vf () const;
+  typedef detail::T<detail::Idd, 'B'> B;
+  typedef detail::T<B, 'D'> D;
 
-    void * operator new (size_t);
-    void * operator new[] (size_t);
-    void operator delete (void *);
-    void operator delete[] (void *);
+} // namespace tracked
 
-    private:
-
-      bool pillaged;
-      void nopillage (char const *) const;
-
-      static struct Reg { std::set<B const *> s; ~Reg (); } reg;
-  };
-
-  std::ostream & operator<< (std::ostream &, B const &);
-
-  struct D: B
-  {
-    D ();
-    D (D const &);
-    #ifdef __GXX_EXPERIMENTAL_CXX0X__
-      D (D &&);
-      D & operator= (D &&);
-    #endif
-    D & operator= (D const &);
-    void vf () const;
-    virtual ~D ();
-  };
-
-  std::ostream & operator<< (std::ostream &, D const &);
-
-#ifdef __GXX_EXPERIMENTAL_CXX0X__
-  void swap(B & a, B & b);
-  void swap(B && a, B & b);
-  void swap(B & a, B && b);
-
-  void swap(D & a, D & b);
-  void swap(D && a, D & b);
-  void swap(D & a, D && b);
-#endif
-}
+#endif // header guard
