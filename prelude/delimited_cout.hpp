cb8525eda4ee9e6c9701cac90e6cb01478d716d4
Eelis van der Weegen <eelis@eelis.net>
2007-08-21 14:10:55 +0200

    delimited_cout.hpp: Fixes for IO manipulators.
    darcs-hash:20070821121055-ec8c9-672459aae1ca02431abbe961a97ffd81f27e9d84.gz

--- prelude/delimited_cout.hpp
+++ prelude/delimited_cout.hpp
@@ -12,13 +12,14 @@ namespace delimited_cout_detail
     template <typename T> C operator<< (T const & t) const { std::cout << ", " << t; return C(); }
     C operator<< (std::ostream & (* const f) (std::ostream &)) const { std::cout << f; return C(); }
     C operator<< (std::ios_base & (* const f) (std::ios_base &)) const { std::cout << f; return C(); }
-      // Last one for things like std::hex which shouldn't be delimited.
   };
 
-  enum Del { del }; // Enum instead of class is header-friendly.
+  enum Del { del };
   template <typename T> C operator<< (Del, T const & t) { std::cout << t; return C(); }
-  C operator<< (Del, std::ostream & (* const f) (std::ostream &)) { std::cout << f; return C(); }
+  Del operator<< (Del, std::ostream & (* const f) (std::ostream &)) { std::cout << f; return del; }
+  Del operator<< (Del, std::ios_base & (* const f) (std::ios_base &)) { std::cout << f; return del; }
 
+  // The <<'s taking std::ios_base&(std::ios_base&)'s are for things like std::hex.
   // The <<'s taking std::ostream&(std::ostream&)'s are for things like std::endl.
 
   Del operator<< (std::ostream &, Del) { return del; }
