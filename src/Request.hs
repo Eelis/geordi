dcd05d1b21b778e8ebd42f9dc94718ac9991a3b3
Eelis van der Weegen <eelis@eelis.net>
2007-12-13 14:58:19 +0100

    Request.hs: Polishing.
    darcs-hash:20071213135819-ec8c9-ea7dccaa50489feee1f9c790ab957fdc2b2dcc3c.gz

--- src/Request.hs
+++ src/Request.hs
@@ -1,4 +1,4 @@
-module Request (parse_request, is_request, prepare_evaluator) where
+module Request (is_request, prepare_evaluator) where
 
 import qualified EvalCxx
 
@@ -12,14 +12,14 @@ import Control.Applicative ((<*>))
 import Prelude hiding (catch, (.))
 import Util
 
-data RequestOpt = RO_compileOnly | RO_terse | RO_help | RO_version deriving Eq
+data Opt = CompileOnly | Terse | Help | Version deriving Eq
 
-requestOptsDesc :: [OptDescr RequestOpt]
-requestOptsDesc =
-  [ Option "c" ["compile-only"] (NoArg RO_compileOnly) undefined
-  , Option "t" ["terse"] (NoArg RO_terse) undefined
-  , Option "h" ["help"] (NoArg RO_help) undefined
-  , Option "v" ["version"] (NoArg RO_version) undefined
+optsDesc :: [OptDescr Opt]
+optsDesc =
+  [ Option "c" ["compile-only"] (NoArg CompileOnly) undefined
+  , Option "t" ["terse"] (NoArg Terse) undefined
+  , Option "h" ["help"] (NoArg Help) undefined
+  , Option "v" ["version"] (NoArg Version) undefined
   ]
 
 wrapPrePost :: String -> String -> String
@@ -39,7 +39,7 @@ is_request botnick botaltnick txt = either (const Nothing) Just (parse p "" txt)
 
 parse_request :: Monad m => String -> m (String {- code -}, Bool {- also run -})
 parse_request req = do
-  (opts, rest) <- case getOpt RequireOrder requestOptsDesc (words req) of
+  (opts, rest) <- case getOpt RequireOrder optsDesc (words req) of
     (_, _, (err:_)) -> fail err
     (opts, non_opts, []) -> return (opts, concat $ takeBack (length non_opts) $ wordsWithWhite req)
       -- We can't use non_opts' contents, because whitespace between tokens has been lost.
@@ -47,14 +47,14 @@ parse_request req = do
     opt = (`elem` opts)
     code = unlines $
       ["#include \"prelude.h\""] ++
-      (if opt RO_terse then ["#include \"terse.hpp\""] else []) ++
+      (if opt Terse then ["#include \"terse.hpp\""] else []) ++
       case () of
-        _ | opt RO_help -> [wrapPrint "help"]
-        _ | opt RO_version -> [wrapPrint $ "\"g++ (GCC) \" << __VERSION__"]
-        _ | '{':_ <- rest -> [wrapStmts rest]
-        _ | '<':'<':x <- rest -> [wrapPrint x]
-        _ -> [rest]
-    also_run = opt RO_help || opt RO_version || not (opt RO_compileOnly)
+        ()| opt Help -> [wrapPrint "help"]
+        ()| opt Version -> [wrapPrint $ "\"g++ (GCC) \" << __VERSION__"]
+        ()| '{':_ <- rest -> [wrapStmts rest]
+        ()| '<':'<':x <- rest -> [wrapPrint x]
+        ()| otherwise -> [rest]
+    also_run = opt Help || opt Version || not (opt CompileOnly)
   return (code, also_run)
 
 prepare_evaluator :: IO (String -> IO String)
@@ -62,6 +62,6 @@ prepare_evaluator = do
   EvalCxx.cap_fds
   gxx : flags <- words . (full_evaluate =<< readFile "compile-config")
     -- readFile would fail after the chroot, hence full_evaluate.
-  return $ either return ((filter (isPrint .||. (== '\n')) . show .) . uncurry (EvalCxx.evaluate gxx (["prelude.a", "-lmcheck"] ++ flags))) . Request.parse_request
+  return $ either return ((filter (isPrint .||. (== '\n')) . show .) . uncurry (EvalCxx.evaluate gxx (["prelude.a", "-lmcheck"] ++ flags))) . parse_request
     -- filtering using isPrint works properly because (1) EvalCxx.evaluate returns a proper Unicode String, not a load of bytes; and (2) to print filtered strings we will use System.IO.UTF8's hPutStrLn which properly UTF-8 encodes the filtered String.
     -- Possible problem: terminals which have not been (properly) UTF-8 configured might interpret bytes that are part of UTF-8 encoded characters as control characters.
