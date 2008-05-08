4d6641d2e891e1071bad355e78d7343f15548fbb
Eelis van der Weegen <eelis@eelis.net>
2008-05-08 23:37:57 +0200

    Testsuite: divide tests into categories. Fix some tests. Add some tests.
    darcs-hash:20080508213757-ec8c9-e906d124ba27352f5355fa14277030da4cd5a92d.gz

--- src/Testsuite.hs
+++ src/Testsuite.hs
@@ -1,10 +1,13 @@
 import qualified Request
 
 import Control.Exception ()
-import Control.Monad (when)
+import Control.Monad (when, forM_)
 import Control.Monad.Error ()
+import System.Environment (getArgs)
 import Text.Regex (matchRegex, mkRegex)
 import Data.List (sort, isPrefixOf)
+
+-- Ascii art:
 
 esc :: Char
 esc = '\x1b'
@@ -18,123 +21,129 @@ green = colored 32
 yellow = colored 33
 cyan = colored 36
 
-class Show t => Test t where do_test :: t -> String -> Bool
+box :: String -> String
+box s = '+' : bar ++ "+\n| " ++ s ++ " |\n+" ++ bar ++ "+"
+  where bar = replicate (length s + 2) '-'
+
+-- Infrastructure:
+
+class Show t => TestPred t where testPred :: t -> String -> Bool
 
 newtype ExactMatch = ExactMatch String deriving Show
-instance Test ExactMatch where do_test (ExactMatch s) = (== s)
+instance TestPred ExactMatch where testPred (ExactMatch s) = (== s)
 
 newtype PrefixMatch = PrefixMatch String deriving Show
-instance Test PrefixMatch where do_test (PrefixMatch s) = isPrefixOf s
+instance TestPred PrefixMatch where testPred (PrefixMatch s) = isPrefixOf s
 
 newtype RegexMatch = RegexMatch String deriving Show
-instance Test RegexMatch where do_test (RegexMatch r) s = matchRegex (mkRegex r) s /= Nothing
+instance TestPred RegexMatch where testPred (RegexMatch r) s = matchRegex (mkRegex r) s /= Nothing
 
 data NoOutput = NoOutput deriving Show
-instance Test NoOutput where do_test NoOutput = (== "")
+instance TestPred NoOutput where testPred NoOutput = (== "")
+
+data Test = Test { test_name, test_request :: String, test_pred :: String -> Bool, test_pred_name :: String }
+
+test :: TestPred p => String -> String -> p -> Test
+test n r p = Test n r (testPred p) (show p)
 
 main :: IO ()
 main = do
 
   evalRequest <- Request.evaluator
 
-  let
-    test :: Test t => String -> String -> t -> IO ()
-    test title req t = do
-      putStrLn $ "\nTest: " ++ yellow title
-      putStrLn $ "Request: " ++ cyan req
-      out <- evalRequest req
-      let success = do_test t out
-      putStrLn $ "Output: " ++ (if success then green else red) (if out == "" then "<none>" else out)
-      when (not success) $ putStr "Expected: " >> print t
-
   putStrLn $
-    "\nNote: In several tests, output is expected to include an error (sometimes on a separate line), so seeing an error in a test's output does not mean the test failed. A test failed if its output is colored red."
+    "\nNote: In several tests, output is expected to include an error (sometimes on a separate line), so seeing an error in a test's output does not mean the test failed. A test failed if its output is colored red.\n"
 
-  test "Simple output" "<< 3" $ ExactMatch "3"
+  let default_test_sets = ["resources", "misc", "diagnostics", "utilities", "errorfilters"]
 
-  test "Stack overflow" "<< f(3); int f(int i) { if (i % 10000 == 0) cout << '+' << flush; return -f(++i); }" $
+  args <- getArgs
+  forM_ (case args of [] -> default_test_sets; _ -> args) $ \set -> do
+    putStrLn $ box $ "Test set: " ++ set
+    forM_ (tests set) $ \(Test n r p pn) -> do
+      putStrLn $ "\nTest: " ++ yellow n
+      putStrLn $ "Request: " ++ cyan r
+      out <- evalRequest r
+      let success = p out
+      putStrLn $ "Output: " ++ (if success then green else red) (if out == "" then "<none>" else out)
+      when (not success) $ putStrLn $ "Expected: " ++ pn
+    putStrLn ""
+
+-- Actual test:
+
+tests :: String -> [Test]
+
+tests "resources" =
+  [ test "Program timeout" "{ for(;;) ; }" $ RegexMatch "Killed"
+  , test "Stack overflow" "<< f(3); int f(int i) { if (i % 10000 == 0) cout << '+' << flush; return -f(++i); }" $
     RegexMatch "\\++ Segmentation fault"
-
-  test "close()" "{ for(int i = 0; i != 1024; ++i) assert(i == 1 || i == 2 || close(i) == -1); }" NoOutput
-
-  test "srand()/time()" "{ srand(time(0)); }" NoOutput
-
-  test "Working directory" "<< get_current_dir_name()" $ ExactMatch "/"
-
-  test "File creation" "{ ofstream f (\"bla\"); assert(errno == EACCES); }" $ ExactMatch ""
-
-  test "-t/-c" "-tc use ns boost; tmpl <tpn T> cls C { expl C (C co &); pvt: stc dub d; pub: void op() (); };" $
-    ExactMatch "Success"
-
-  let quine = "{string t,u,y(1,34);stringstream i(\"{string t,u,y(1,34);stringstream i(!);getline(i,t,'!')>>u;cout<<t<<y<<i.str()<<y<<u;}\");getline(i,t,'!')>>u;cout<<t<<y<<i.str()<<y<<u;}"
-  test "Quine" quine $ ExactMatch quine
-
-  test "-v" "-v" $ PrefixMatch "g++ (GCC) 4"
-
-  test "getopt" "-monkey chicken" $ ExactMatch "unrecognized option `-m'\n"
-
-  test "Program timeout" "{ for(;;) ; }" $ RegexMatch "Killed"
-
-  test "Demangled printable typeid" "<< typeid(int)" $ ExactMatch "int"
-
-  test "Custom assert()/abort()" "{ assert(4 > 9); }" $ ExactMatch "Assertion `4 > 9' fails.\nAborted."
-
-  test "File I/O" "{ { ofstream f (__FILE__); f << \"foo\"; } cout << ifstream(__FILE__).rdbuf(); }" $
+  , test "Open FDs" "{ for(int i = 0; i != 1024; ++i) assert(i == 1 || i == 2 || close(i) == -1); }" NoOutput
+  , test "File creation" "{ ofstream f (\"bla\"); assert(errno == EACCES); }" $ NoOutput
+  , test "Working directory" "<< get_current_dir_name()" $ ExactMatch "/"
+  , test "File I/O" "{ { ofstream f (__FILE__); f << \"foo\"; } cout << ifstream(__FILE__).rdbuf(); }" $
     ExactMatch "foo"
-
-  test "Memory limit" "{ int i = 0; while (new (nothrow) char [1 << 20]) ++i; assert(i < 250); }" NoOutput
-
-  test "Fd limit" "{ int i = 0; while (open(__FILE__, 0) != -1) ++i; assert(errno == EMFILE); assert(i < 50); }  extern \"C\" int open (char const *, int);" NoOutput
-
-  test "File size limit" "{ ofstream f (__FILE__); string meg (1 << 20, 'x'); for (;;) { f << meg << flush; cout << '+' << flush; } }" $
+  , test "Memory limit" "{ int i = 0; while (new (nothrow) char [1 << 20]) ++i; assert(i < 250); }" NoOutput
+  , test "Fd limit" "{ int i = 0; while (open(__FILE__, 0) != -1) ++i; assert(errno == EMFILE); assert(i < 50); }  extern \"C\" int open (char const *, int);" NoOutput
+  , test "File size limit" "{ ofstream f (__FILE__); string meg (1 << 20, 'x'); for (;;) { f << meg << flush; cout << '+' << flush; } }" $
     RegexMatch "\\+{1,50} File size limit exceeded$"
-
-  test "Signal" "{ int x = 0; cout << 3 / x; }" $
-    ExactMatch "Floating point exception"
-
-  test "System call interception" "<< fork()" $
-    RegexMatch "SYS_[^:]*: Operation not permitted"
-
-  test "-mcheck diagnostic" "{ int * p = new int [3]; p[3] = 6; delete[] p; }" $
-    PrefixMatch "memory clobbered past end of allocated block\n"
-
-  test "Ditto" "{ int * p = new int [3]; p[-1] = 6; delete[] p; }" $
-    PrefixMatch "memory clobbered before allocated block\n"
-
-  test "Checking global allocation/deallocation operators" "{ delete new int[3]; }" $
-    PrefixMatch "error: tried to apply non-array operator delete to pointer returned by new[].\n"
-
-  let s = "dicekjhbagfl" in
-    test "Nontrivial program (Brainfuck interpreter)" ("{b(program);}char program[]=\">>,[>>,]<<[[-<+<]>[>[>>]<[.[-]<[[>>+<<-]<]>>]>]<<]\",input[]=\"" ++ s ++ "\", *i=input,m[512]={},*p=m;void b(char*c){for(;*c&&*c!=']';++c){(*((p+=*c=='>')-=*c=='<')+=*c=='+') -=*c=='-';*c=='.'&&cout<<*p;if(*c==',')*p=*i++;if(*c=='['){for(++c;*p;)b(c);for(int d=0;*c!=']'||d--;++c)d+=*c=='[';}}}") $ ExactMatch (sort s)
-
-  test "Compiler timeout" "-c #include __FILE__" $
-    ExactMatch "g++: Killed"
-
-  test "bin IO manipulator" "<< showbase << uppercase << bin << setfill('_') << internal << setw(10) << 53" $
-    ExactMatch "0B__110101"
-
-  test "Tracked" "{ using tracked::B; B x(0), y(x); x.B::~B(); x.f(); }" $
-    ExactMatch "B0*(0) B1*(B0) B0~ error: tried to call B::f() on destructed B0.\nAborted."
-
-  test "Custom terminate() handler" "{ throw std::logic_error(\"It is not logical, Captain.\"); }" $
-    ExactMatch "logic_error: It is not logical, Captain.\nAborted."
-
-  test "UTF-8 handling" "<< 'a' << char(144) << 'b' << char(215) << char(144) << 'c'" $
-    ExactMatch "a�bאc"
-
-  test "Error filters" "{ wistringstream is; !is.str(); }" $
-    ExactMatch "error: no match for 'operator!' in '!wistringstream::str() const()'"
-
-  test "Ditto" "<< ETYPE(&vector<queue<istream_iterator<int> > >::foo)" $
-    ExactMatch "error: 'foo' is not a member of 'vector<queue<istream_iterator<int>>>'"
-
-  test "-fstack-protector-all" "{ char buf [10]; fill(buf, buf+30, 'x'); }" $
-    PrefixMatch "*** stack smashing detected ***: /t terminated\n"
-
-  test "ETYPE" "{ int i = 4; cout << ETYPE(++i); }" $ ExactMatch "lvalue int"
-
-  test "Recursive exec()"
+  , test "System call interception" "<< fork()" $ RegexMatch "SYS_[^:]*: Operation not permitted"
+  , test "Signal" "{ int x = 0; cout << 3 / x; }" $ ExactMatch "Floating point exception"
+  , test "Recursive exec()"
     "int main (int const argc, char const * const * argv) { string s; if (argc >= 2) s = argv[1]; s += 'x'; if (s.size() % 100 == 0) cout << '+' << flush; execl(\"/t\", \"/t\", s.c_str(), 0); }" $
     RegexMatch "\\++( Killed)?$"
+  ]
 
-  test "Range printing" "{ vector<int> v; v += 3, 5, 9, 4, 1; cout << v; }" $ ExactMatch "[3, 5, 9, 4, 1]"
+tests "misc" =
+  [ test "Simple output" "<< 3" $ ExactMatch "3"
+  , test "Compiler timeout" "-c #include __FILE__" $ ExactMatch "g++: Killed"
+  , let quine = "{string t,u,y(1,34);stringstream i(\"{string t,u,y(1,34);stringstream i(!);getline(i,t,'!')>>u;cout<<t<<y<<i.str()<<y<<u;}\");getline(i,t,'!')>>u;cout<<t<<y<<i.str()<<y<<u;}" in test "Quine" quine $ ExactMatch quine
+  , test "UTF-8 handling" "<< 'a' << char(144) << 'b' << char(215) << char(144) << 'c'" $
+    ExactMatch "a�bאc"
+  , let s = "dicekjhbagfl" in
+    test "Nontrivial program (Brainfuck interpreter)" ("{b(program);}char program[]=\">>,[>>,]<<[[-<+<]>[>[>>]<[.[-]<[[>>+<<-]<]>>]>]<<]\",input[]=\"" ++ s ++ "\", *i=input,m[512]={},*p=m;void b(char*c){for(;*c&&*c!=']';++c){(*((p+=*c=='>')-=*c=='<')+=*c=='+') -=*c=='-';*c=='.'&&cout<<*p;if(*c==',')*p=*i++;if(*c=='['){for(++c;*p;)b(c);for(int d=0;*c!=']'||d--;++c)d+=*c=='[';}}}") $ ExactMatch (sort s)
+  , test "srand()/time()" "{ srand(time(0)); }" NoOutput
+  , test "-v" "-v" $ PrefixMatch "g++ (GCC) 4"
+  , test "getopt" "-monkey chicken" $ ExactMatch "unrecognized option `-m'\n"
+  ]
+
+tests "diagnostics" =
+  [ test "-fstack-protector-all" "{ char buf [10]; fill(buf, buf+30, 'x'); }" $
+    PrefixMatch "*** stack smashing detected ***: /t terminated\n"
+  , test "-mcheck diagnostic" "{ int * p = new int [3]; p[3] = 6; delete[] p; }" $
+    PrefixMatch "memory clobbered past end of allocated block\n"
+  , test "Ditto" "{ int * p = new int [3]; p[-1] = 6; delete[] p; }" $
+    PrefixMatch "memory clobbered before allocated block\n"
+  , test "Checking global allocation/deallocation operators" "{ delete new int[3]; }" $
+    PrefixMatch "error: tried to apply non-array operator delete to pointer returned by new[].\n"
+  ,  test "Custom terminate() handler" "{ throw std::logic_error(\"It is not logical, Captain.\"); }" $
+    ExactMatch "terminated by logic_error: It is not logical, Captain.\nAborted."
+  ]
+
+tests "tracked" =
+  [ test "Operation on destructed B" "{ using tracked::B; B x(0), y(x); x.B::~B(); x.f(); }" $ ExactMatch "B0*(0) B1*(B0) B0~ error: tried to call B::f() on destructed B0.\nAborted."
+  , test "Re-destruction" "{ using tracked::B; B b; b.~B(); }" $ ExactMatch "B0* B0~ B0~ error: tried to re-destruct destructed B0.\nAborted."
+  , test "Stack leak" "{ using tracked::B; { union { double d; char c[sizeof(B)]; }; new (c) B; } B b; }" $ ExactMatch "B0* B1* B1~ leaked: [B0] \nAborted."
+  , test "Stack overwrite" "{ using tracked::B; B b; new (&b) B; }" $ ExactMatch "B0* error: leaked: B0\nAborted."
+  , test "new[]/delete[]." "{ boost::shared_array<tracked::B> a(new tracked::D[2]); }" $ ExactMatch "new(D[]) B0* D0* B1* D1* D1~ B1~ D0~ B0~ delete[D0, D1]"
+  , test "Operation on non-existent object" "{ tracked::B * p = 0; p->f(); }" $ ExactMatch "error: tried to call B::f() on non-existent object.\nAborted."
+  ]
+
+tests "utilities" =
+  [ test "-t/-c" "-tc use ns boost; tmp <tpn T> cls C { expl C (C co &); pvt: stc dub d; pub: void op() (); };" $
+    ExactMatch "Success"
+  , test "ETYPE" "{ int i = 4; cout << ETYPE(++i); }" $ ExactMatch "lvalue int"
+  , test "Range printing" "{ vector<int> v; v += 3, 5, 9, 4, 1; cout << v; }" $ ExactMatch "[3, 5, 9, 4, 1]"
+  , test "Demangled printable typeid" "<< typeid(int)" $ ExactMatch "int"
+  , test "Custom assert()/abort()" "{ assert(4 > 9); }" $ ExactMatch "Assertion `4 > 9' fails.\nAborted."
+  ,  test "bin IO manipulator" "<< showbase << uppercase << bin << setfill('_') << internal << setw(10) << 53" $ ExactMatch "0B__110101"
+  ]
+
+tests "errorfilters" =
+  [ test "Error filters" "{ wistringstream is; !is.str(); }" $
+    ExactMatch "error: no match for 'operator!' in '!wistringstream::str() const()'"
+  , test "Ditto" "<< ETYPE(&vector<queue<istream_iterator<int> > >::foo)" $
+    ExactMatch "error: 'foo' is not a member of 'vector<queue<istream_iterator<int>>>'"
+  , test "Preprocessor error" "<< 08" $ ExactMatch "error: invalid digit \"8\" in octal constant"
+  , test "[with ...]-replacement" "{ cout << 1 == 1; }" $ ExactMatch "error: no match for 'operator==' in 'cout. ostream::operator<<(1) == 1'"
+  ]
+
+tests s = error $ "no such test set: " ++ s
