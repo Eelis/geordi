3da2c311f391fc8aa871505a8865191886c38e3e
Eelis van der Weegen <eelis@eelis.net>
2008-09-13 23:32:08 +0200

    prelude/bin_iomanip: Don't use free store allocation, and add a refs-forwarding constructor.
    darcs-hash:20080913213208-ec8c9-2d233db9c9dbb734fa23ae33dc0fee5ec4bb086c.gz

--- prelude/bin_iomanip.hpp
+++ prelude/bin_iomanip.hpp
@@ -56,6 +56,7 @@ See ISO 14882 [22.2.2.2.2].
 #include <stdexcept>
 #include <iterator>
 #include <algorithm>
+#include <climits>
 
 template <typename Ch = char, typename Out = std::ostreambuf_iterator<Ch> >
 class bin_num_put: public std::num_put<Ch, Out>
@@ -80,7 +81,8 @@ class bin_num_put: public std::num_put<Ch, Out>
     IB::fmtflags const f = io.flags();
     if (f & IB::basefield) return base::do_put(i, io, fill, v);
 
-    std::basic_string<Ch> core;
+    std::size_t const buf_size = sizeof(I) * CHAR_BIT * 2;
+    Ch r[buf_size], * p = r + buf_size;
 
     {
       std::numpunct<char> const & numpunct = std::use_facet<std::numpunct<char> >(io.getloc());
@@ -94,21 +96,21 @@ class bin_num_put: public std::num_put<Ch, Out>
       {
         if (g != grouping.end() && group_len++ == *g)
         {
-          core += numpunct.thousands_sep(); group_len = 1;
+          *--p = numpunct.thousands_sep(); group_len = 1;
           if (++g == grouping.end()) --g;
         }
-        core += '0' + bool(v & m);
+        *--p = '0' + bool(v & m);
       }
       while ((m <<= 1) && v >= m);
     }
 
     std::streamsize const w = io.width(); io.width(0);
-    std::streamsize d = core.size(); if (f & IB::showbase) d += 2;
+    std::streamsize d = r + buf_size - p; if (f & IB::showbase) d += 2;
     IB::fmtflags const adj = f & IB::adjustfield;
     if (adj != IB::left && adj != IB::internal) while (w > d) { *i++ = fill; ++d; }
     if (f & IB::showbase) { *i++ = '0'; *i++ = f & IB::uppercase ? 'B' : 'b'; }
     if (adj == IB::internal) while (w > d) { *i++ = fill; ++d; }
-    i = std::copy(core.rbegin(), core.rend(), i);
+    i = std::copy(p, r + buf_size, i);
     if (adj == IB::left) while (w > d) { *i++ = fill; ++d; }
     return i;
   }
@@ -120,6 +122,10 @@ class bin_num_put: public std::num_put<Ch, Out>
   }
 
   // The "proper" thing to do is to add a static std::locale::id const id, but that way bin_num_put does not replace the std::num_put facet in the locale.
+
+  public:
+
+    explicit bin_num_put(std::size_t const refs = 0): base(refs) {}
 };
 
 template <typename Ch, typename Tr>
@@ -134,8 +140,6 @@ std::basic_ios<Ch, Tr> & bin (std::basic_ios<Ch, Tr> & io)
   io.setf(std::ios_base::fmtflags(0), std::ios_base::basefield);
   return io;
 }
-
-void imbue_bin_num_put (std::ostream &);
 
 #endif // Header guard.
 
