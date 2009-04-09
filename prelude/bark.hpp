e2bf6bd0dff60be4dee1567d1569abaa79ecab3f
Eelis van der Weegen <eelis@eelis.net>
2009-04-10 01:37:12 +0200

    prelude: Make BARK more robust. (thanks litb)
    Ignore-this: bf2802a6c4dd0cf192c93885e3a949a6
    
    darcs-hash:20090409233712-ec8c9-c9018ebc9d4965ae6ca65c63a49ed34187fa8d40.gz

---
+++ prelude/bark.hpp
@@ -0,0 +1,71 @@
+#ifndef BARK_HPP
+#define BARK_HPP
+
+#include <vector>
+#include <utility>
+#include <string>
+#include <iostream>
+#include <cstring>
+#include "geordi.hpp"
+#include <boost/range.hpp>
+
+#include "more_ostreaming.hpp"
+
+namespace bark_detail
+{
+  template <typename T, typename I>
+  I find_first_toplevel(T const open, T const close, T const v, I i, I const e) {
+    for(int nested = 0; i != e && !(nested == 0 && *i == v); ++i)
+    { if(*i == open) ++nested; if(*i == close) --nested; }
+    return i;
+  }
+
+  template <std::size_t N>
+  char const * bark(char const (& s)[N]) {
+    char const * r = 0;
+    for(std::reverse_iterator<char const *> i(find_first_toplevel('<', '>', '(', s, s+N)), e(s); i != e; ) {
+      i = find_first_toplevel('>', '<', ' ', i+1, e);
+      if(!r) r = i.base();
+      if(std::strstr(i.base(), "operator ") == i.base() || std::strstr(i.base(), "::operator "))
+        return i.base();
+    }
+    return r;
+  }
+}
+
+#ifndef BARK_TEST
+
+#define BARK (::std::cout << geordi::parsep << bark_detail::bark(__PRETTY_FUNCTION__) << ::geordi::parsep)
+  // We don't include a trailing semicolon, because a function body looking like { BARK } confuses geordi's C++ parser.
+
+#else
+
+#include <sstream>
+#include <cassert>
+
+#define BARK(expected) \
+  { std::string const s(bark_detail::bark(__PRETTY_FUNCTION__)); std::cout << s << '\n'; assert(s == expected); }
+
+#include <boost/function.hpp>
+#include <boost/implicit_cast.hpp>
+
+void * operator new(std::size_t, char) throw() { BARK("operator new(size_t, char)"); return 0; }
+boost::function<void()> f() { BARK("f()"); return 0; }
+std::pair<int, int> g() { BARK("g()"); return std::make_pair(0, 0); }
+struct X { operator unsigned int() { BARK("X::operator unsigned int()"); return 0; } };
+
+signed char h(void(*)()) { BARK("h(void (*)())"); return 0; }
+
+int main()
+{
+  BARK("main()");
+  f(); g(); h(0);
+  boost::implicit_cast<unsigned int>(X());
+  ::operator new(0, 'x');
+
+  std::cout << "-----------------\nNo test failures.\n";
+}
+
+#endif // tests
+
+#endif // header guard
--- prelude/prelude.hpp
+++ prelude/prelude.hpp
@@ -18,6 +18,7 @@
 #include "show.hpp"
 #include "evil_casts.hpp"
 #include "geordi.hpp"
+#include "bark.hpp"
 
 #include <stdint.h>
 
@@ -161,26 +162,6 @@ namespace geordi { geordi::initializer_t const initializer; }
 #define assert(e) ((e) ? void() : (void(::std::cout << "Assertion `" #e "' fails."), ::std::fclose(stdout), ::std::abort()))
 
 #define typeid(...) static_cast< ::type_strings_detail::type_info const &>(typeid(__VA_ARGS__))
-
-namespace bark_detail
-{
-  template <typename T, typename I>
-  I find_first_toplevel(T const open, T const close, T const v, I i, I const e) {
-    for(int nested = 0; i != e && !(nested == 0 && *i == v); ++i)
-    { if(*i == open) ++nested; if(*i == close) --nested; }
-    return i;
-  }
-
-  template <std::size_t N>
-  void bark(char const (& s)[N]) {
-    std::reverse_iterator<char const*> const i(std::find(RANGE(s), '(')), e(s);
-    std::cout << geordi::parsep << find_first_toplevel('>', '<', ' ', i, e).base() << geordi::parsep << std::flush;
-      // We strip the return type and decl-specifiers.
-  }
-}
-
-#define BARK bark_detail::bark(__PRETTY_FUNCTION__)
-  // We don't include a trailing semicolon, because a function body looking like { BARK } confuses geordi's C++ parser.
 
 using namespace std;
 using namespace boost::assign;
