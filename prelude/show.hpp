e8a754b2aa3a86e3cb26a7e5e968db6eb331e0b9
Eelis van der Weegen <eelis@eelis.net>
2009-09-12 14:11:13 +0200

    prelude: Make SHOW also work without "cout <<".
    Ignore-this: 57d127664436d71fa839948d1c80faf2
    
    darcs-hash:20090912121113-ec8c9-890e311ebe228b301e7f8cf7a4290316875a608e.gz

--- prelude/show.hpp
+++ prelude/show.hpp
@@ -5,17 +5,64 @@
 
 namespace show_detail
 {
-  template <typename T> struct shower { T const & v; char const * const s; };
+  template <typename T> struct shower
+  {
+    T const & v;
+    char const * const s;
+    mutable bool due;
+
+    shower(T const & v, char const * const s): v(v), s(s), due(true) {}
+    shower(shower const & o): v(o.v), s(o.s), due(o.due) { o.due = false; }
+    ~shower() { if(due) std::cout << *this; }
+
+    private: void operator=(shower const&);
+  };
 
   template <typename C, typename Tr, typename T>
   std::basic_ostream<C, Tr> & operator<<(std::basic_ostream<C, Tr> & o, shower<T> const & s)
-  { return o << s.s << " = " << s.v; }
+  { s.due = false; return o << s.s << " = " << s.v; }
 
-  template <typename T>
-  shower<T> show(T const & v, char const * const s) { shower<T> const r = { v, s }; return r; }
+  template <typename T, typename U>
+  shower<U> const & operator,(shower<T> const & x, shower<U> const & y)
+  {
+    if(x.due) std::cout << x;
+    std::cout << ", " << y;
+    return y;
+  }
+
+  template <typename T> shower<T> show(T const & v, char const * const s) { return shower<T>(v, s); }
 }
 
 #define SHOW(x) ::show_detail::show((x), #x)
-  // The more obvious   #define SHOW(x) #x " = " << (x)   does not work in   cout << SHOW(x), SHOW(y);. The alternative   #define SHOW(x) (#x " = " + ::boost::lexical_cast<::std::string>(x))   does not use the stream's formatting flags.
 
 #endif // header guard
+
+#ifdef SHOW_TEST
+
+#include <sstream>
+#include <cassert>
+#include <iomanip>
+
+template <typename Ch, typename Tr, typename T>
+inline std::basic_ostream<Ch, Tr> & operator, (std::basic_ostream<Ch, Tr> & o, T const & t)
+{ return o << ", " << t; }
+
+int main()
+{
+  using std::cout;
+
+  int const x = 20; double y = 1.3; char z = 'z';
+  std::hex(cout);
+  std::ostringstream o;
+  cout.rdbuf(o.rdbuf());
+
+  cout << SHOW(x), SHOW(y), SHOW(z);
+  cout << '\n';
+  SHOW(x), SHOW(y), SHOW(z);
+
+  cout.rdbuf(0);
+
+  assert(o.str() == "x = 14, y = 1.3, z = z\nx = 14, y = 1.3, z = z");
+}
+
+#endif // testing
