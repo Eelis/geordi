a2bddeefc253bea3aceb6a686ca273a8a2b46501
Eelis van der Weegen <eelis@eelis.net>
2010-07-17 18:55:13 +0200

    prelude/prelude: Use MB_LEN_MAX instead of magic literal.

--- prelude/prelude.cpp
+++ prelude/prelude.cpp
@@ -112,7 +112,7 @@ namespace geordi
 
 std::ostream & operator<<(std::ostream & o, wchar_t const c)
 {
-  char buf[4];
+  char buf[MB_LEN_MAX];
   int const i = wctomb(buf, c);
   if(i < 0) o << '?';
   else std::copy(buf, buf + i, std::ostreambuf_iterator<char>(o));
