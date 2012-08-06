47f7e0a4e618fbda972f0948103878456310b9c7
Eelis van der Weegen <eelis@eelis.net>
2012-08-06 05:38:58 +0200

    prelude/bark: Add special case for lambdas.

--- prelude/bark.hpp
+++ prelude/bark.hpp
@@ -6,9 +6,13 @@
 #include <cstring>
 #include <cassert>
 #include <string>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/find.hpp>
 
 namespace bark_detail
 {
+  using std::string;
+
   char closer(char const c) {
     switch(c) {
       case '<': return '>';
@@ -34,14 +38,13 @@ namespace bark_detail
     }
   }
 
-  char const * bark(char const * const pretty_function, std::string const func) {
-    char const * p = std::strstr(pretty_function, (func + '(').c_str());
-    assert(p);
-    if(p != pretty_function && *(p-1) == ':') {
-      std::reverse_iterator<char const *> j(p), e(pretty_function);
-      return find_toplevel(' ', j, e).base();
+  string bark(string const pf, string const func) {
+    string::const_iterator p = boost::ends_with(pf, ")>") ? pf.end() : boost::find_first(pf, func + '(').begin();
+    if(p == pf.end() || (p != pf.begin() && *(p-1) == ':')) {
+      std::reverse_iterator<string::const_iterator> j(p);
+      p = find_toplevel(' ', j, pf.rend()).base();
     }
-    else return p;
+    return string(p, pf.end());
   }
 }
 
@@ -50,7 +53,7 @@ namespace bark_detail
 #include "geordi.hpp"
 
 #define BARK \
-  (::std::printf("%s%s%s", ::geordi::parsep, ::bark_detail::bark(__PRETTY_FUNCTION__, __func__), ::geordi::parsep), ::std::fflush(stdout))
+  (::std::printf("%s%s%s", ::geordi::parsep, ::bark_detail::bark(__PRETTY_FUNCTION__, __func__).c_str(), ::geordi::parsep), ::std::fflush(stdout))
   // We don't include a trailing semicolon, because a function body looking like { BARK } confuses geordi's C++ parser.
 
 #else
@@ -67,7 +70,7 @@ namespace bark_detail
 #include <boost/function.hpp>
 #include <boost/implicit_cast.hpp>
 
-void * operator new(std::size_t, char) throw() { BARK("operator new(size_t, char)"); return 0; }
+void * operator new(std::size_t, char) throw() { BARK("operator new(std::size_t, char)"); return 0; }
 boost::function<void()> f() { BARK("f()"); return 0; }
 std::pair<int, int> g() { BARK("g()"); return std::make_pair(0, 0); }
 struct X { operator unsigned int() { BARK("X::operator unsigned int()"); return 0; } void f(){ BARK("X::f()");} };
@@ -76,6 +79,7 @@ std::vector<unsigned int> vf(){ BARK("vf()"); return std::vector<unsigned int>()
 template<typename A, typename B> struct Y { void f(){ BARK("Y<A, B>::f() [with A = int, B = int]"); } };
 signed char h(void(*)()) { BARK("h(void (*)())"); return 0; }
 template<void(*)(X,X)> void bla(){ BARK("bla() [with void (* <anonymous>)(X, X) = operator<<]"); }
+
 
 int main()
 {
@@ -86,6 +90,7 @@ int main()
   f(); g(); h(0);
   boost::implicit_cast<unsigned int>(X());
   ::operator new(0, 'x');
+  [](int){ BARK("main()::<lambda(int)>"); }(3);
   bla<&operator<< >();
   X()<<X();
 
