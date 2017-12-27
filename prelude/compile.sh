76956fd73a4bf1f106ead03ba26f3c049bab6ff4
Eelis van der Weegen <eelis@eelis.net>
2017-12-27 20:13:39 +0100

    Don't repeat ubsan flags in prelude/compile.sh.

--- prelude/compile.sh
+++ prelude/compile.sh
@@ -8,7 +8,7 @@ do
   cp prelude.hpp /geordi/run/prelude-${std}.hpp
   $GCC $stdflag $GCC_COMPILE_FLAGS -c -x c++-header prelude.hpp -o /geordi/run/prelude-${std}.hpp.gch
   $CLANG $stdflag $CLANG_COMPILE_FLAGS -x c++-header prelude.hpp -o /geordi/run/prelude-${std}.hpp.pch
-  $GCC $stdflag $GCC_COMPILE_FLAGS -fsanitize=undefined -fno-sanitize-recover=all -fPIC -shared -o /lib/libgeordi_preload-${std}.so preload.cpp
-  $GCC $stdflag $GCC_COMPILE_FLAGS -fsanitize=undefined -fno-sanitize-recover=all -c {prelude,tracked,more_ostreaming,bark}.cpp
+  $GCC $stdflag $GCC_COMPILE_FLAGS -fPIC -shared -o /lib/libgeordi_preload-${std}.so preload.cpp
+  $GCC $stdflag $GCC_COMPILE_FLAGS -c {prelude,tracked,more_ostreaming,bark}.cpp
   ar -rsc /lib/libgeordi_prelude-${std}.a {prelude,bark,tracked,more_ostreaming}.o
 done
